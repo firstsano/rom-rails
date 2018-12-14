ddd9778b1cf926ea02b01797b5e4e281af46599a
Alexander Koltunov <firstsano@gmail.com>
2018-12-14 12:02:20 +0400

    Add relation

---
+++ app/relations/volgaspot/services_relation.rb
@@ -0,0 +1,32 @@
+module Volgaspot
+  class ServicesRelation < ::ROM::Relation[:http]
+    gateway :volgaspot
+
+    schema(:volgaspot_services) do
+      attribute :id, ::Types::Strict::Int
+      attribute :type_sid, ::Types::String
+      attribute :utm5_service_id, ::Types::Strict::Int
+      attribute :type, ::Types::Coercible::Hash.schema(
+        id: ::Types::Strict::Int,
+        name: ::Types::String,
+        utm5_type_id: ::Types::Int,
+        utm5_template_id: ::Types::Int
+      )
+
+      primary_key :id
+    end
+
+    def by_id(id)
+      add_params('id[]' => id)
+    end
+
+    def all
+      add_params(scope: 'all')
+    end
+
+    def base
+      with_base_path('services')
+        .add_params(expand: 'type')
+    end
+  end
+end
