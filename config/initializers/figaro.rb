284dd1dab7df8165db4d1dea01d3e435a51eeb9d
Alexander Koltunov <firstsano@gmail.com>
2019-02-28 15:29:26 +0400

    Add yandex related keys to Figaro's require

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,6 @@
-Figaro.require_keys('volgaspot_api', 'utm', 'company', 'exception_recipients')
+required_keys = %w[
+  volgaspot_api utm company exception_recipients
+  yandex_till_api yandex_till_username yandex_till_password
+]
+
+Figaro.require_keys required_keys
