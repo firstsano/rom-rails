3d7099687c710eea2b04e55e3f6e92b7624e8bca
Alexander Koltunov <firstsano@gmail.com>
2019-04-05 14:37:41 +0400

    Remove restriction from payment hash

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -14,39 +14,5 @@ class Payment < ::ROM::Struct
   attribute :ext_payment_date, ::Types::Form::DateTime
   attribute :status, ::Types::String
 
-  attribute :ext_payment, ::Types::Strict::Hash.schema(
-    id: ::Types::Strict::String,
-    status: ::Types::Strict::String,
-    paid: ::Types::Strict::Bool,
-    payment_method: ::Types::Strict::Hash,
-    created_at: ::Types::Form::DateTime,
-    test: ::Types::Strict::Bool,
-
-    amount: ::Types::Hash.schema(
-      value: ::Types::Form::Float,
-      currency: ::Types::String
-    ),
-
-    confirmation: ::Types::Coercible::Hash.schema(
-      type: ::Types::Strict::String,
-      enforce: ::Types::Bool.optional,
-      return_url: ::Types::String.optional,
-      confirmation_url: ::Types::String.optional
-    ).default({}),
-
-    cancellation_details: ::Types::Coercible::Hash.schema(
-      party: ::Types::Strict::String,
-      reason: ::Types::Strict::String
-    ).default({}),
-
-    authorization_details: ::Types::Coercible::Hash.schema(
-      rrn: ::Types::Strict::String,
-      auth_code: ::Types::Strict::String
-    ).default({}),
-
-    description: ::Types::String.optional,
-    captured_at: ::Types::Form::DateTime.optional,
-    expires_at: ::Types::Form::DateTime.optional,
-    metadata: ::Types::Coercible::Hash.default({})
-  )
+  attribute :ext_payment, ::Types::Strict::Hash
 end
