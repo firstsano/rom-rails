97b98b532125b498b7574766b192094630ab105d
Alexander Koltunov <firstsano@gmail.com>
2019-04-05 17:29:40 +0400

    Remove deprecated application keys from requirements, delete deprecated code

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,6 +1,3 @@
-required_keys = %w[
-  volgaspot_api utm company exception_recipients
-  yandex_till_api yandex_till_username yandex_till_password
-]
+required_keys = %w[volgaspot_api utm company exception_recipients]
 
 Figaro.require_keys required_keys
--- config/initializers/rom.rb
+++ config/initializers/rom.rb
@@ -10,16 +10,4 @@ ROM::Rails::Railtie.configure do |config|
     request_handler: Gateways::Volgaspot::RequestHandler.new,
     response_handler: Gateways::Volgaspot::ResponseHandler.new
   }]
-
-  username = ENV.fetch('yandex_till_username')
-  password = ENV.fetch('yandex_till_password')
-  config.gateways[:yandex_till] = [:http, {
-    uri: ENV.fetch('yandex_till_api'),
-    headers: {
-      Accept: 'application/json',
-      'Content-Type' => 'application/json'
-    },
-    request_handler: Gateways::YandexTill::RequestHandler.new(username, password),
-    response_handler: Gateways::YandexTill::ResponseHandler.new
-  }]
 end
